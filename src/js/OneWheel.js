9038b2a069e7589073532028ded1db86597baba4
Brian Bugert <coolbots7@gmail.com>
2020-06-17 19:36:30 -0700

    Create OneWheel class to handle communication

--- src/js/OneWheel.js
+++ src/js/OneWheel.js
@@ -34,43 +34,200 @@ const OW_CHARACTERISTICS = {
     TRIP_TOTAL_AMPHOURS: 'e659f313ea9811e3ac100800200c9a66',
     UART_SERIAL_READ: 'e659f3feea9811e3ac100800200c9a66',
     UART_SERIAL_WRITE: 'e659f3ffea9811e3ac100800200c9a66'
-}
+};
 
+class OneWheel {
 
-function createAuthenticationResponse(serialReadBytes) {
-    var authenticationResponse = null;
+    constructor(peripheral) {
+        this.peripheral = peripheral;
 
-    //Create authentication hash
-    const hashSerialBytes = serialReadBytes.slice(3, 19);
-    const hashConst = Buffer.from([0xD9, 0x25, 0x5F, 0x0F, 0x23, 0x35, 0x4E, 0x19, 0xBA, 0x73, 0x9C, 0xCD, 0xC4, 0xA9, 0x17, 0x65]);
-    const hashInput = Buffer.concat([hashSerialBytes, hashConst]);
-    const hashOutput = md5.digest(hashInput);
-    authenticationResponse = Buffer.concat([Buffer.from([0x43, 0x52, 0x58]), Buffer.from(hashOutput)]);
+        this.allCharacteristics = undefined;
 
-    //create authentication check byte
-    var checkByte = 0x00;
-    for (var i = 0; i < authenticationResponse.length; i++) {
-        checkByte = authenticationResponse[i] ^ checkByte;
+        this.serialReadBuffer = Buffer.alloc(20);
+        this.serialReadBufferSize = 0;
+        this.sendKey = true;
+
+        this.authenticated = false;
+
+        this._onConnect = this._onConnect.bind(this);
+        this._onDisconnect = this._onDisconnect.bind(this);
+        this._onRSSIUpdate = this._onRSSIUpdate.bind(this);
+
+        this.peripheral.once('connect', this._onConnect);
+        this.peripheral.once('disconnect', this._onDisconnect);
+        this.peripheral.once('rssiUpdate', this._onRSSIUpdate);
     }
-    authenticationResponse = Buffer.concat([authenticationResponse, Buffer.from([checkByte])]);
 
-    return authenticationResponse;
-}
+    connect() {
+        this.peripheral.connect();
+    }
 
+    //Get Peripheral Info
+    getID() {
+        return this.peripheral.id;
+    }
 
-function unsignedInt(buffer) {
-    if (buffer === undefined || buffer.length <= 0) {
-        return 0
+    getAddress() {
+        return this.peripheral.address;
     }
-    else if (buffer.length === 1) {
-        return buffer[0];
+
+    getLocalName() {
+        return this.peripheral.advertisement.localName;
     }
-    else {
-        return (buffer[0] << 8) + buffer[1];
+
+    getTXPowerLevel() {
+        return this.peripheral.advertisement.txPowerLevel;
     }
-}
+
+    getRSSI() {
+        return this.peripheral.advertisement.rssi;
+    }
+
+    //Get Characteristics
+    async getBatteryRemaining() {
+        return this._unsignedInt(await this._getCharacteristic(OW_CHARACTERISTICS.BATTERY_REMAINING).readAsync());
+    }
+
+    async getLifetimeOdometer() {
+        return this._unsignedInt(await this._getCharacteristic(OW_CHARACTERISTICS.LIFETIME_ODOMETER).readAsync());
+    }
+
+    //Event Handlers
+    _onConnect(error) {
+        if (error) {
+            console.log("connect error: ", error);
+            return;
+        }
+
+        console.log("connected");
+
+        this.peripheral.discoverSomeServicesAndCharacteristics([OW_SERVICE_UUID], [], (error, services, characteristics) => {
+            if (error) {
+                console.log("error getting services and characteristics:", error);
+                return;
+            }
+
+            this.allCharacteristics = characteristics;
+
+            console.log("services and characteristics discovered");
+
+            //get firmware version characteristic value       
+            const firmwareCharacteristic = this._getCharacteristic(OW_CHARACTERISTICS.FIRMWARE_VERSION);
+            firmwareCharacteristic.read((error, firmwareVersion) => {
+
+                //TODO check firmware eversion >= gemini
+
+                //enable notifications on serial read characteristic
+                const serialReadCharacteristic = this._getCharacteristic(OW_CHARACTERISTICS.UART_SERIAL_READ);
+
+                serialReadCharacteristic.subscribe((error) => {
+                    if (error) {
+                        console.log("serial read subscribe error: ", error);
+                    }
+
+                    //write firmware version characteristic
+                    firmwareCharacteristic.write(firmwareVersion, false, (error) => {
+                        if (error) {
+                            console.log('firmware characteristic write error: ', error);
+                        }
+                    });
+
+                });
+
+                serialReadCharacteristic.on('data', (data, isNotification) => {
+
+                    //get serial read bytes
+                    this.serialReadBuffer.fill(data, this.serialReadBufferSize);
+                    this.serialReadBufferSize += data.length;
+                    if (this.serialReadBufferSize >= 20 && this.sendKey) {
+
+                        //create authentication hash
+                        const authenticationResponse = this._createAuthenticationResponse(this.serialReadBuffer);
+
+                        //write authentication hash to serial write characteristic
+                        const serialWriteCharacteristic = this._getCharacteristic(OW_CHARACTERISTICS.UART_SERIAL_WRITE);
+                        serialWriteCharacteristic.write(authenticationResponse, false, (error) => {
+                            if (error) {
+                                console.log("serial write error: ", error);
+                                return;
+                            }
+                            this.sendKey = false;
+                            this.authenticated = true;
+
+                            console.log("authenticated");
+
+                            //Write firmware back to OW regularly to keep authentication valid
+                            setInterval(() => {
+                                firmwareCharacteristic.write(firmwareVersion, false, (error) => {
+                                    if (error) {
+                                        console.log('firmware characteristic write error: ', error);
+                                    }
+                                });
+                            }, 5000);
+
+                        });
+                    };
+
+                    //disable notifications on serial read characteristic
+                    serialReadCharacteristic.unsubscribe((error) => {
+                        if (error) {
+                            console.log("unsubscribe error: ", error);
+                        }
+                    });
+
+                }); // end on serial read data
+
+            }); // end firmware read
+
+        }); // end discover services and characteristics
+
+    }
+
+    _onDisconnect() {
+        console.log("disconnected from peripheral: ", this.peripheral.advertisement.localName);
+    }
+
+    _onRSSIUpdate(rssi) {
+        console.log("RSSI:", rssi);
+    }
+
+    //Helper Functions
+    _getCharacteristic(uuid) {
+        return this.allCharacteristics.find(characteristic => characteristic.uuid === uuid);
+    }
+
+    _createAuthenticationResponse(serialReadBytes) {
+        var authenticationResponse = null;
+
+        //Create authentication hash
+        const hashSerialBytes = serialReadBytes.slice(3, 19);
+        const hashConst = Buffer.from([0xD9, 0x25, 0x5F, 0x0F, 0x23, 0x35, 0x4E, 0x19, 0xBA, 0x73, 0x9C, 0xCD, 0xC4, 0xA9, 0x17, 0x65]);
+        const hashInput = Buffer.concat([hashSerialBytes, hashConst]);
+        const hashOutput = md5.digest(hashInput);
+        authenticationResponse = Buffer.concat([Buffer.from([0x43, 0x52, 0x58]), Buffer.from(hashOutput)]);
+
+        //create authentication check byte
+        var checkByte = 0x00;
+        for (var i = 0; i < authenticationResponse.length; i++) {
+            checkByte = authenticationResponse[i] ^ checkByte;
+        }
+        authenticationResponse = Buffer.concat([authenticationResponse, Buffer.from([checkByte])]);
+
+        return authenticationResponse;
+    }
+
+    _unsignedInt(buffer) {
+        if (buffer === undefined || buffer.length <= 0) {
+            return 0
+        }
+        else if (buffer.length === 1) {
+            return buffer[0];
+        }
+        else {
+            return (buffer[0] << 8) + buffer[1];
+        }
+    }
+};
 
 module.exports.OW_SERVICE_UUID = OW_SERVICE_UUID;
-module.exports.OW_CHARACTERISTICS = OW_CHARACTERISTICS;
-module.exports.createAuthenticationResponse = createAuthenticationResponse;
-module.exports.unsignedInt = unsignedInt;
+module.exports.OneWheel = OneWheel;
--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,13 @@ var express = require("express");
 const OneWheel = require('./OneWheel');
 
 var app = express();
+//TODO add port environment variable
 const PORT = 3000;
 
-//Globals
-var serialReadBuffer = Buffer.alloc(20);
-var serialReadBufferSize = 0;
-var sendKey = true;
+//TODO add mac address environment variable
+//TODO if no mac address environment variable, default to looking for service
 
-var allCharacteristics = undefined;
-const getCharacteristic = (uuid) => {
-  return allCharacteristics.find(characteristic => characteristic.uuid === uuid);
-}
-
+var onewheel = null;
 
 noble.on('stateChange', function (state) {
   console.log("state changed:", state);
@@ -28,105 +23,10 @@ noble.on('stateChange', function (state) {
 
 noble.on('discover', function (peripheral) {
   noble.stopScanning();
-  console.log("Peripheral discovered: ", peripheral.advertisement.localName);
+  console.log("OW Peripheral discovered: ", peripheral.advertisement.localName);
 
-  peripheral.connect();
-
-  peripheral.on('disconnect', () => {
-    console.log("disconnected from peripheral: ", peripheral.advertisement.localName);
-  })
-
-  peripheral.on('rssiUpdate', (rssi) => {
-    console.log("RSSI:", rssi);
-  });
-
-  peripheral.once('connect', (error) => {
-    if (error) {
-      console.log("connect error: ", error);
-      return;
-    }    
-
-    console.log("connected");
-
-    peripheral.discoverSomeServicesAndCharacteristics([OneWheel.OW_SERVICE_UUID], [], (error, services, characteristics) => {
-      if (error) {
-        console.log("error getting services and characteristics:", error);
-        return;
-      }
-
-      console.log("services and characteristics discovered");
-
-      allCharacteristics = characteristics;
-
-      //get firmware version characteristic value       
-      const firmwareCharacteristic = getCharacteristic(OneWheel.OW_CHARACTERISTICS.FIRMWARE_VERSION);
-      firmwareCharacteristic.read((error, firmwareVersion) => {
-
-        //TODO check firmware eversion >= gemini
-
-        //enable notifications on serial read characteristic
-        const serialReadCharacteristic = getCharacteristic(OneWheel.OW_CHARACTERISTICS.UART_SERIAL_READ);
-
-        serialReadCharacteristic.subscribe((error) => {
-          if (error) {
-            console.log("serial read subscribe error: ", error);
-          }
-
-          //write firmware version characteristic
-          firmwareCharacteristic.write(firmwareVersion, false, (error) => {
-            if (error) {
-              console.log('firmware characteristic write error: ', error);
-            }
-          });
-
-        });
-
-        serialReadCharacteristic.on('data', (data, isNotification) => {
-
-          //get serial read bytes
-          serialReadBuffer.fill(data, serialReadBufferSize);
-          serialReadBufferSize += data.length;
-          if (serialReadBufferSize >= 20 && sendKey) {
-
-            //create authentication hash
-            const authenticationResponse = OneWheel.createAuthenticationResponse(serialReadBuffer);
-
-            //write authentication hash to serial write characteristic
-            const serialWriteCharacteristic = getCharacteristic(OneWheel.OW_CHARACTERISTICS.UART_SERIAL_WRITE);
-            serialWriteCharacteristic.write(authenticationResponse, false, (error) => {
-              if (error) {
-                console.log("serial write error: ", error);
-              }
-              sendKey = false;
-
-              console.log("authenticated");
-
-              //Write firmware back to OW regularly to keep authentication valid
-              setInterval(() => {
-                firmwareCharacteristic.write(firmwareVersion, false, (error) => {
-                  if (error) {
-                    console.log('firmware characteristic write error: ', error);
-                  }
-                });
-              }, 5000);
-
-            });
-          };
-
-          //disable notifications on serial read characteristic
-          serialReadCharacteristic.unsubscribe((error) => {
-            if (error) {
-              console.log("unsubscribe error: ", error);
-            }
-          });
-
-        }); // end on serial read data
-
-      }); // end firmware read
-
-    }); // end discover services and characteristics
-
-  }); // end connect
+  onewheel = new OneWheel.OneWheel(peripheral);
+  onewheel.connect();
 
 }); //end discover
 
@@ -139,9 +39,9 @@ app.get("/onewheel", async (req, res, next) => {
   var batteryRemaining = 0;
   var lifetimeOdometer = 0;
 
-  if (allCharacteristics) {
-    batteryRemaining = OneWheel.unsignedInt(await getCharacteristic(OneWheel.OW_CHARACTERISTICS.BATTERY_REMAINING).readAsync());
-    lifetimeOdometer = OneWheel.unsignedInt(await getCharacteristic(OneWheel.OW_CHARACTERISTICS.LIFETIME_ODOMETER).readAsync());
+  if(onewheel) {
+    batteryRemaining = await onewheel.getBatteryRemaining();
+    lifetimeOdometer = await onewheel.getLifetimeOdometer();
   }
 
   res.send({ 'batteryRemaining': batteryRemaining, 'lifetimeOdometer': lifetimeOdometer })
